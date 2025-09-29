9cccae4687b1d7ead260019738322fcdffefa18b
Pavel Merekin <merekin33@gmail.com>
2025-09-29 13:34:26 +0700

    particles bg added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Major_Mono_Display, VT323 } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-    variable: '--font-geist-sans',
-    subsets: ['latin'],
+const majorMono = Major_Mono_Display({
+    weight: '400',
+    variable: '--font-major',
 })
 
-const geistMono = Geist_Mono({
-    variable: '--font-geist-mono',
-    subsets: ['latin'],
+const vt323 = VT323({
+    weight: '400',
+    variable: '--font-vt',
 })
 
 export const metadata: Metadata = {
@@ -24,7 +24,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body
-                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                className={`${majorMono.variable} ${vt323.variable} antialiased bg-black w-dvw h-dvh`}
             >
                 {children}
             </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import ParticlesBackground from '@/components/ui/ParticlesBackground'
+
 export default function HomePage() {
-    return <div>home page</div>
+    return (
+        <div className="relative w-full h-full">
+            <ParticlesBackground
+                particleColors={['#ffffff', '#ffffff']}
+                particleCount={1000}
+                particleSpread={30}
+                speed={0.04}
+                particleBaseSize={140}
+                moveParticlesOnHover={false}
+                alphaParticles={false}
+                disableRotation={false}
+            />
+
+            {/* дальше основные экраны идут */}
+        </div>
+    )
 }
